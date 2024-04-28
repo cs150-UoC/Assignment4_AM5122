d7ac3ec5f6f21391d271f9862494b88c5ecfa966
STEFANOS <csd5122@csd.uoc.gr>
2024-04-28 17:41:14 +0300

    sixthcommit

--- chapter.12.3.cpp
+++ chapter.12.3.cpp
@@ -58,22 +58,21 @@ class ImageData {
     ImageData(string ppmname) {
         string filepath = "C:\\Users\\steka\\Desktop\\CPP-PP-Edu-main\\Resources\\Code\\Programming-code\\Chapter12\\PPM Images\\" + ppmname;
         fstream fileppm(filepath);
-        fileppm << "\n";
-        fileppm.seekg(-1,std::ios::cur);
-        fileppm >> width >> height;
-        fileppm << "\n";
-        fileppm.seekg(-1,std::ios::cur);
-        fileppm << "\n";
-        fileppm.seekg(-1,std::ios::cur);
-        int r,g,b;
+        string line;
+        getline(fileppm,line);
+        getline(fileppm,line);
+        istringstream ss(line);
+        ss >> width >> height;
+        getline(fileppm,line);
+        double r,g,b;
         for(int i = 0; i<=height-1; i++) {
             vector<Pixel> newrow;
             Image.push_back(newrow);
             for(int j = 0; j<=width-1; j++) {
-                fileppm >> r >> g >> b;
-                fileppm << "\n";
-                fileppm.seekg(-1,std::ios::cur);
-                Image[i].push_back(Pixel(j,i,vec3(r,g,b)));
+                getline(fileppm,line);
+                istringstream ss(line);
+                ss >> r >> g >> b;
+                Image[i].push_back(Pixel((j+1) * 20 - 10,(i+1) * 20 - 10,vec3(r,g,b)));
             }
         }
         fileppm.close();
@@ -84,19 +83,20 @@ int main()
 {
     using namespace Graph_lib;   // our graphics facilities are in Graph_lib
      
-    string ppmname = "world.ppm";
+    string ppmname = "simple.ppm";
     class ImageData ppmimage = ImageData(ppmname);
 
-    Point tl(100,100);           // to become top left  corner of window
+    Point tl(100,100);       // to become top left  corner of window
 
     Simple_window win(tl,ppmimage.getwidth() * 20 + 70,ppmimage.getheight() * 20,ppmname);    // make a simple window
 
     vector<vector<Pixel>> Image = ppmimage.getImage();
     for(int i = 0; i<=ppmimage.getheight() - 1; i++) {
         for(int j = 0; j<=ppmimage.getwidth() - 1; j++) {
-            Fl_Color c = fl_rgb_color(Image[i][j].getcolor().x(),Image[i][j].getcolor().y(),Image[i][j].getcolor().z());
+            Fl_Color c = fl_rgb_color(uchar(Image[i][j].getcolor().x()),uchar(Image[i][j].getcolor().y()),uchar(Image[i][j].getcolor().z()));
             Rectangle r(Point(Image[i][j].getpositionx(),Image[i][j].getpositiony()),20,20);
-            r.set_fill_color(c);
+            Color C = Color(int(c));
+            r.set_fill_color(C);
             win.attach(r);
         }
     }
